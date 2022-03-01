f54bdf13fff4a84351e6f76638d3bd5b728b795b
Randika Abeyrathna <randika.l.abeyrathna@gmail.com>
2022-03-01 18:45:05 +0530

    app routing for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UserRegistrationComponent} from "./components/user-registration/user-registration.component";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
-  {path:"register",component:UserRegistrationComponent }
+  {path:"register",component:UserRegistrationComponent },
+  {path:"",component:LoginComponent }
 ];
 
 @NgModule({
