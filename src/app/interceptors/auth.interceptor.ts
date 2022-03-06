faf86b256e63a1e2d72a9f98dc65f53731130e8c
Randika Abeyrathna <randika.l.abeyrathna@gmail.com>
2022-03-06 22:22:37 +0530

    integrate Interceptors

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,25 @@
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {LoginServiceService} from "../service/login-service.service";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{
+
+  constructor(private loginService:LoginServiceService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    const userData = this.loginService.userInfo.getValue();
+    if (userData){
+        const newRequest =req.clone({
+          headers:req.headers.set("Authorization",`Bearer ${userData.access_token}`)
+        });
+        return next.handle(newRequest)
+    }
+    return next.handle(req);
+
+  }
+
+}
